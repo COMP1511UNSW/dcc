c112fe7224d37b9c232162b56185df10cd3e160c
Andrew Taylor <andrewt@unsw.edu.au>
2020-11-23 20:54:33 +1100

    fix bug in handling of valgrind errors from posix_spawn parameters

---
+++ tests/run_time_errors/uninitialized_posix_spawnp.c
@@ -0,0 +1,8 @@
+//dcc_flags=-fsanitize=valgrind
+#include <spawn.h>
+int main(void) {
+	pid_t p;
+	char *a[] = {"/bin/true", NULL};
+	posix_spawn_file_actions_t file_actions;
+	posix_spawnp(&p, a[0], &file_actions, NULL, a, NULL);
+}
