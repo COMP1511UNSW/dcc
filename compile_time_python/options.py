COMP1511UNSW/dcc

commit 2ac318bc2a53f3d2815c0544ac4882d48a92eb04

assume C++ if asked to compile a .cpp file makes test scripts easier, and might help novices

compile_time_python/options.py
@@ -381,7 +381,7 @@ def parse_arg(arg, remaining_args, options):
     elif arg == "--help":
         print()
         sys.exit(0)
-    elif arg.startswith("-o"):
+    elif arg == "-x":
         if arg == "-o":
             if remaining_args:
                 options.object_pathname = remaining_args.pop(0)
@@ -436,6 +436,8 @@ def process_possible_source_file(pathname, options, processed_files):
     if extension.lower() in [".a", ".o", ".so"]:
         options.object_files_being_linked = True
         return
+    if extension.lower() in [".cpp", ".c++"]:
+        options.cpp_mode = True
     try:
         with open(pathname, encoding="utf-8", errors="replace") as f:
             for line in f:

tests/do_tests.sh
@@ -9,16 +9,11 @@ done
 
 export tests_dir="$(dirname $(readlink -f $0))"
 export dcc="$(readlink -f ${1:-"./dcc"})"
-export dcc_cpp="${dcc}++"
 export c_compiler="${2:-clang}"
 export cpp_compiler="${3:-clang++}"
 
 command -v "$dcc" > /dev/null || {
 	echo "$0: error: $dcc not found"
-	exit 1
-}
-command -v "$dcc_cpp" > /dev/null || {
-	echo "$0: warning: $dcc_cpp not found - no C++ tests will be run"
 	exit 1
 }
 

tests/single_test.sh
@@ -48,29 +48,23 @@ REMOVE_NON_DETERMINATE_VALUES='
 compile_options_list=$(egrep '^//dcc_flags=' "$src_file"|sed 's?//??;s/ /#/g')
 compile_options_list=${compile_options_list:-'dcc_flags=""'}
 
+case "$src_file" in
+*.cpp) compiler="$cpp_compiler";;
+*) compiler="$c_compiler"
+esac
+	
 for compile_options in $compile_options_list
 do
 	rm -f a.out tmp.actual_*
 	compile_options=$(echo "$compile_options"|sed 's/#/ /g')
 	case "$src_file" in
-	*.c)
+	*.c|*.cpp)
 		dcc_flags=
 		suffix=`echo $compile_options|sed 's/^dcc_flags=//;s/ /_/g;s/["$]//g;s/src_file//;s?/?_?g'`
 		eval $compile_options
 		expected_output_basename="`basename $src_file .c`$suffix"
-		#echo "$dcc" --c-compiler=$c_compiler $dcc_flags "$src_file"
-		"$dcc" --c-compiler=$c_compiler $dcc_flags "$src_file" 2>tmp.actual_stderr >/dev/null
-		test ! -s tmp.actual_stderr && DCC_DEBUG=1 ./a.out </dev/null   2>>tmp.actual_stderr >tmp.actual_stdout
-		;;
-
-	*.cpp)
-		command -v "$dcc_cpp" > /dev/null || continue
-		dcc_flags=
-		suffix=`echo $compile_options|sed 's/^dcc_flags=//;s/ /_/g;s/["$]//g;s/src_file//;s?/?_?g'`
-		eval $compile_options
-		expected_output_basename="`basename $src_file`$suffix"
-		#echo "$dcc" --c-compiler=$cpp_compiler $dcc_flags "$src_file"
-		"$dcc_cpp" --c-compiler=$cpp_compiler $dcc_flags "$src_file" 2>tmp.actual_stderr >/dev/null
+		#echo "$dcc" --c-compiler=$compiler $dcc_flags "$src_file"
+		"$dcc" --c-compiler=$compiler $dcc_flags "$src_file" 2>tmp.actual_stderr >/dev/null
 		test ! -s tmp.actual_stderr && DCC_DEBUG=1 ./a.out </dev/null   2>>tmp.actual_stderr >tmp.actual_stdout
 		;;
 

commit 4bfcdd300ebebada97370f8019bbfa3f0fd07238

fix bug with -o introduced in 2ac318b

compile_time_python/options.py
@@ -381,7 +381,7 @@ def parse_arg(arg, remaining_args, options):
     elif arg == "--help":
         print()
         sys.exit(0)
-    elif arg == "-x":
+    elif arg.startswith("-o"):
         if arg == "-o":
             if remaining_args:
                 options.object_pathname = remaining_args.pop(0)

tests/run_time_errors/unterminated_strcat.c
@@ -1,5 +1,4 @@
 #include <string.h>
-#include <unistd.h>
 
 int main(void) {
     char c = 'd';